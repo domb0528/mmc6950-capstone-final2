177ff4f626e98e399d6f1fd91c4504b9253bc20d
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-07-30 13:53:05 -0600

    fixed changes added login and signup feautures

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,13 +1,17 @@
 import styles from './style.module.css'
+import useLogout from "../../hooks/useLogout";
 import Link from 'next/link'
 
-export default function Header() {
+export default function Header(props) {
+  const logout = useLogout();
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <Link href="/" className={styles.logo}>Sip & Savor 🍷 </Link>
-        <Link href="/search">Search</Link>
-      </div>
-    </header>
-  )
+    <div className={styles.container}>
+      <Link href="/" className={styles.logo}>Sip & Savor 🍷 </Link>
+      <Link href="/search">Search</Link>
+      <Link href="/login">Login</Link>
+      <Link href="/signup">Sign Up</Link>
+    </div>
+  </header>
+)
 }
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,20 +1,12 @@
 import '../styles/globals.css'
 import Header from '../components/header'
 import Footer from '../components/footer'
-import { Open_Sans } from '@next/font/google'
-
-const openSans = Open_Sans({
-  weight: ['300', '400', '800'],
-  style: ['normal', 'italic'],
-  subsets: ['latin']
-})
 
 function MyApp({ Component, pageProps }) {
-  return <div className={[openSans.className, "app-container"].join(" ")}>
-    <Header />
-    <Component {...pageProps} />
-    <Footer />
+  return <div><Header />
+  <Component {...pageProps} />
+  <Footer />
   </div>
 }
-
 export default MyApp
+

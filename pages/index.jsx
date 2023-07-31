10346994ccd80cdacd1e193600c73043615ebcda
Dominick birge <dominickbirge@Dominicks-MacBook-Pro.local>
2023-07-31 17:32:39 -0600

    fixed changes hoping deployment works

--- pages/index.jsx
+++ pages/index.jsx
@@ -6,20 +6,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Sip & Savor 🍷</title>
+        <title>Calorie Watcher 🏋️‍♂️</title>
         <meta name="description" content="Sip & Savor is a wine pairing app" />
-        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍷</text></svg>"/>
+        <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🏋️‍♂️</text></svg>"/>
       </Head>
       <main>
         <div className={styles.container}>
-          <h1>Welcome to Sip & Savor 🍷 where you can find the best food and wine pairings</h1>
+          <h1>Welcome to Calorie Watcher 🏋️‍♂️ where you can find the best meals for weight loss</h1>
           <section className={styles.jumbo}>
-            <p>Find a Pairing. 🍷</p>
+            <p>Find a meal plan 🏋️‍♂️</p>
           </section>
         </div>
         <section className={styles.goSearch}>
           <p></p>
-          <Link href="/search" className="button">Search for your Pairing</Link>
+          <Link href="/search" className="button">Search for your suggested meals</Link>
         </section>
       </main>
     </>

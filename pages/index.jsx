c6f5d1be9c95bd9dd3e53509ccb62edb5730cdd2
Dominick Birge <94083462+domb0528@users.noreply.github.com>
2023-08-06 12:38:38 -0600

    Update index.jsx
    Deleted a function.

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,8 +13,7 @@ export default function Home() {
       <main>
         <div className={styles.container}>
           <h1>Welcome to Sip & Savor 🍷 where you can find the best food and wine pairings</h1>
-          <section className={styles.jumbo}>
-            <p>Find a Pairing. 🍷</p>
+          <section className={styles.jumbo}
           </section>
         </div>
         <section className={styles.goSearch}>
